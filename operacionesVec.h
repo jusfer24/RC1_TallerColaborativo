9d73b62f9fc26b186237c0dca6e992203319528c
Justin <jusfer125@gmail.com>
2024-06-04 13:10:48 -0500

    new

--- operacionesVec.h
+++ operacionesVec.h
@@ -3,5 +3,4 @@ void imprimirLibros(char[][50], int[], float[], int);
 void editarLibro(char[][50], int[], float[], int);
 void agregarLibro(char[][50], int[], float[], int);
 void borrarLibro(char[][50], int[], float[], int);
-int leerEnteroEntre(char* , int , int );
 void mostrarmenu();
